0f8f8565693dab32e3771d603b5dcf9aef7c59c4
Danny <djycheng@bitbucket.org>
2015-05-02 16:20:13 -0700

    find TSP path with MST, no color

--- MSTalg.py
+++ MSTalg.py
@@ -1,4 +1,5 @@
 import util
+from collections import deque
 
 parent = dict()
 rank = dict()
@@ -23,26 +24,55 @@ def union(vertice1, vertice2):
             if rank[root1] == rank[root2]: rank[root2] += 1
 
 def MSTalg(graph):
-  edges = []
-  visited = []
-  for i in range(graph.numCities):
-    make_set(i)
-    visited.append(i)
-    for node, dist in enumerate(graph.edges[i]):
-      if (node != i) and (node not in visited):
-        edges.append((dist, i, node))
-    minimum_spanning_tree = set()
-    edges.sort()
-  for edge in edges:
-        weight, vertice1, vertice2 = edge
+    edges = []
+    visited = []
+    for i in range(graph.numCities):
+        make_set(i)
+        visited.append(i)
+        for node, dist in enumerate(graph.edges[i]):
+            if (node != i) and (node not in visited):
+                edges.append((dist, i, node, graph.colors[i]))
+        minimum_spanning_tree = set()
+        edges.sort()
+    for edge in edges:
+        weight, vertice1, vertice2, color = edge
         if find(vertice1) != find(vertice2):
             union(vertice1, vertice2)
             minimum_spanning_tree.add(edge)
-  return minimum_spanning_tree
+    return minimum_spanning_tree
+
+def createPath(graph, edges):
+		visited = []
+		path = []
+		
+		stack = []
+		for e in range(len(edges)):
+				if (edges[e][1] == 0 or edges[e][2] == 0):
+						stack.append(edges[e])
+		visited = [0]
+		path = [(0, graph.colors[0])]
+		
+		while len(stack) > 0:
+				edge = stack.pop()
+				if edge[1] in visited and edge[2] in visited:
+						continue
+				if edge[1] not in visited:
+						v = edge[1]
+				elif edge[2] not in visited:
+						v = edge[2]
+				for e in range(len(edges)):
+						if (edges[e][1] == v or edges[e][2] == v):
+								stack.append(edges[e])
+				visited.append(v)
+				path.append((v, graph.colors[v]))
+		return path
+						
+						
+				
 
 T = 1 # number of test cases
 #fout = open ("answer.out", "w")
-for t in xrange(1, T+1):
+for t in xrange(6, 7):
     fin = open(str(t) + ".in", "r")
     N = int(fin.readline())
     d = [[] for i in range(N)]
@@ -53,7 +83,18 @@ for t in xrange(1, T+1):
     graph = util.Graph(N, d, c)
 
     result = MSTalg(graph)
-    print result
+    edges = sorted(list(result), key=lambda x: x[1])
+    length = len(edges)
+    for e in range(length):
+        edges.append((edges[e][0], edges[e][2], edges[e][1], edges[e][3]))
+    result = createPath(graph, edges)
+    print "Edges:"
+    stuff = sorted(edges, key=lambda x: x[1])
+    for x in range(len(stuff)):
+        print stuff[x]
+    print "Result:"
+    for x in range(len(result)):
+        print result[x]
 
 
     # find an answer, and put into assign
